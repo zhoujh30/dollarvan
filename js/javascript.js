aae6b2209b6cf788be3128fbc41964f45850a6df
Jianhao Zhou <jianhaozhou@Jianhaos-MBP.fios-router.home>
2017-04-03 00:24:41 -0400

    update location test and search button

--- js/javascript.js
+++ js/javascript.js
@@ -179,7 +179,74 @@ var AllRoutes = new google.maps.KmlLayer({
           map: map
         });
 
+//Add my location
+// button.onclick = function() {
+//   var startPos;
+//   var element = document.getElementById("nudge");
 
+//   var showNudgeBanner = function() {
+//     nudge.style.display = "block";
+//   };
+
+//   var hideNudgeBanner = function() {
+//     nudge.style.display = "none";
+//   };
+
+//   var nudgeTimeoutId = setTimeout(showNudgeBanner, 5000);
+
+//   var geoSuccess = function(position) {
+//     hideNudgeBanner();
+//     // We have the location, don't display banner
+//     clearTimeout(nudgeTimeoutId);
+
+//     // Do magic with location
+//     startPos = position;
+//     document.getElementById('startLat').innerHTML = startPos.coords.latitude;
+//     document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+//   };
+//   var geoError = function(error) {
+//     switch(error.code) {
+//       case error.TIMEOUT:
+//         // The user didn't accept the callout
+//         showNudgeBanner();
+//         break;
+//       };
+//   };
+
+//   navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+// };
+
+// check for Geolocation support
+// if (navigator.geolocation) {
+//   console.log('Geolocation is supported!');
+// }
+// else {
+//   console.log('Geolocation is not supported for this Browser/OS.');
+// }
+
+// window.onload = function() {
+//   var startPos;
+//   var geoOptions = {
+//     maximumAge: 5 * 60 * 1000,
+//     timeout: 10 * 1000
+//   }
+
+//   var geoSuccess = function(position) {
+//     startPos = position;
+//     document.getElementById('startLat').innerHTML = startPos.coords.latitude;
+//     document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+//   };
+//   var geoError = function(error) {
+//     console.log('Error occurred. Error code: ' + error.code);
+//     // error.code can be:
+//     //   0: unknown error
+//     //   1: permission denied
+//     //   2: position unavailable (error response from location provider)
+//     //   3: timed out
+//   };
+
+//   navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
+// };
 
 //Add my location
 var myloc = new google.maps.Marker({
